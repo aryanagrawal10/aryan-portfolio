8ef808907f9a3d485944c1e21536a03c149cfb98
Aryan Agrawal <aryanagrawal0007@gmail.com>
2025-05-29 03:50:04 -0400

    Add: Comment

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Html,
   Body,
@@ -9,13 +9,13 @@ import {
   Preview,
   Section,
   Text,
-} from "@react-email/components";
-import { Tailwind } from "@react-email/tailwind";
+} from '@react-email/components'
+import { Tailwind } from '@react-email/tailwind'
 
 type ContactFormEmailProps = {
-  message: string;
-  senderEmail: string;
-};
+  message: string
+  senderEmail: string
+}
 
 export default function ContactFormEmail({
   message,
@@ -31,6 +31,7 @@ export default function ContactFormEmail({
             <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
               <Heading className="leading-tight">
                 You received the following message from the contact form
+                {/* Comment to remove later */}
               </Heading>
               <Text>{message}</Text>
               <Hr />
@@ -40,5 +41,5 @@ export default function ContactFormEmail({
         </Body>
       </Tailwind>
     </Html>
-  );
+  )
 }
